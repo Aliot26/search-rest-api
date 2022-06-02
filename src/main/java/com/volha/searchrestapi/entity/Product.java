4a8003fca1e45fa83126f38cd34df0cf6cb1b175
Aliot26 <olgakochanowa@gmail.com>
2022-06-02 15:57:33 +0200

    Created entity Product

---
+++ src/main/java/com/volha/searchrestapi/entity/Product.java
@@ -0,0 +1,35 @@
+package com.volha.searchrestapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/*
+ *Created on 02.06.2022
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sku;
+    private String name;
+    private String description;
+    private boolean active;
+    private String imageUrl;
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @UpdateTimestamp
+    private LocalDateTime dateUpdated;
+}

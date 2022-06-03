912f21744a6ae7a7559ee93216f03e3a55c083d5
Aliot26 <olgakochanowa@gmail.com>
2022-06-03 15:02:06 +0200

    Fixed mistake in query

--- src/main/java/com/volha/searchrestapi/repository/ProductRepository.java
+++ src/main/java/com/volha/searchrestapi/repository/ProductRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
  */
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query("SELECT p FROM Product p WHERE" +
+    @Query("SELECT p FROM Product p WHERE " +
             "p.name LIKE CONCAT ('%', :query, '%')" +
             "OR p.description LIKE CONCAT ('%', :query, '%')")
     List<Product> searchProduct(String query);
